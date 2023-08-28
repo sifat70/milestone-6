7c6280471dc4737a61f032969c4dfb8d643dd239
Sifatul Islam <mdshepatul.islam@gmail.com>
2023-08-28 22:17:19 +0600

    35.5 added

---
+++ module-35/async.js
@@ -0,0 +1,11 @@
+console.log(1);
+console.log(2);
+setTimeout(() => {
+    console.log('lazzy logged')
+},4000);
+console.log(4);
+console.log(5);
+
+function doSomething(){
+    console.log(console.log('khela hobbe'))
+}
---
+++ module-35/context.js
@@ -0,0 +1,9 @@
+let a = 5;
+
+function add(num1, num2) {
+    const result = num1 + num2 + a;
+    return result
+}
+
+const result = add(5, 5)
+console.log(result);
